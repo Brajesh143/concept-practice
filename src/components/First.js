7f39b85a1641f9d98fb97e93c454e164bcbb07a7
Brajesh Singh <brajesh_singh@persistent.com>
2024-05-28 20:05:30 +0530

    add first component

---
+++ src/components/First.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function First() {
+    console.log("hello")
+    return (
+        <>
+        { console.log("hi") }
+            <div>This is first component</div>
+        </>
+    )
+}
